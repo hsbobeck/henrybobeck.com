c67436fc2c4b7d69daf388d77430e492a6bf7821
hsbobeck <hsbipod@gmail.com>
2023-05-12 15:34:46 -0400

    deployment bug fix

--- pages/contact.js
+++ pages/contact.js
@@ -21,7 +21,7 @@ export function ContactContent() {
         <>
             <div className='disable-scroll'>
                 <p className="smalltext mono-font">Hi there!</p>
-                <p className="smalltext mono-font">Please feel free to email me at <a className="link-text weight-bold" href="mailto:contact@henrybobeck.com" target="_blank">contact@henrybobeck.com</a>,<br />
+                <p className="smalltext mono-font">Please feel free to email me at <a className="link-text weight-bold" href="mailto:contact@henrybobeck.com" target="_blank" rel="noreferrer">contact@henrybobeck.com</a>,<br />
                     or by Instagram DM at <a className="link-text weight-bold" href="https://www.instagram.com/henrybobeck" target="_blank" rel="noreferrer">@henrybobeck</a>.</p>
                 <p className="smalltext mono-font"></p>
                 <p className="smalltext mono-font">Thanks, Henry</p>
