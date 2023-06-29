0550024304572ebb7454a83df5f5e3e170bb87b6
hsbobeck <hsbipod@gmail.com>
2023-06-29 14:52:40 -0700

    contact blurb wording

--- pages/contact.js
+++ pages/contact.js
@@ -32,7 +32,7 @@ export function ContactContent() {
                 <div className="padded-item paragraph">
                     <p className="smalltext mono-font">Hi there!</p>
                     <p className="smalltext mono-font">My name is Henry Bobeck.</p>
-                    <p className="smalltext mono-font">I am a seasoned traveler, filmmaker, cinematographer, and photographer from Indiana, USA. While on the road, I’ve learned the power of stories to bring us together and connect us through emotion. What I love most is to communicate emotion through films and photography, and I want to use this passion of mine to tell stories which support us and the life around us. I’d love to tell yours. I’m available for work wherever you are and wherever you’re going.</p>
+                    <p className="smalltext mono-font">I am a traveler, filmmaker, cinematographer, and photographer from Indiana, USA. While on the road, I’ve learned the power of stories to bring us together and connect us through emotion. What I love most is to communicate emotion through films and photography, and I want to use this passion of mine to tell stories which support us and the life around us. I’d love to tell yours. I’m available for work wherever you are and wherever you’re going.</p>
                     <p className="smalltext mono-font">I’m excited to hear from you by:</p>
                     <p className="smalltext mono-font">Email / <a className="link-text weight-bold" href="mailto:contact@henrybobeck.com" target="_blank" rel="noreferrer">contact@henrybobeck.com</a><br />
                         Instagram / <a className="link-text weight-bold" href="https://www.instagram.com/henrybobeck" target="_blank" rel="noreferrer">@henrybobeck</a></p>
