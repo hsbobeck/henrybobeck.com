4db72ac56ce1f449cd7e221dc6857812e7d5e8d1
hsbobeck <hsbipod@gmail.com>
2023-09-07 16:26:51 -0400

    update seo description

--- components/meta.js
+++ components/meta.js
@@ -5,7 +5,7 @@ export default function MetaLayout({ children }) {
         <>
             <Head>
                 <title>Henry Bobeck</title>
-                <meta name="description" content="filmmaker & photographer" />
+                <meta name="description" content="Henry Bobeck is a traveling photographer, filmmaker, and cinematographer." />
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
