0086aced5fd85edf7fb7b986e6bb19f45253af50
hsbobeck <hsbipod@gmail.com>
2023-10-27 18:52:37 +0200

    homepage edit

--- pages/home.js
+++ pages/home.js
@@ -9,12 +9,9 @@ import NextJsImage from '../components/NextJsImage';
 import image1 from "../public/images/photography/personal/scenes-to-remind-me-of-home/1-2.jpg";
 import image2 from "../public/images/shop/moroccan.jpg";
 import image3 from "../public/images/photography/portrait/edem1-43.jpg";
-import image4 from "../public/images/photography/personal/oct-31-2021/6.jpg";
-import image5 from "../public/images/photography/personal/lollapalooza-in-motion/1.jpg";
-import image6 from "../public/images/photography/personal/in-wishes-we-hide-our-pain/horiz3.jpg";
-import image7 from "../public/images/photography/personal/as-they-remember-us/1.jpg";
-import image8 from "../public/images/photography/event/hs-nashville-2023/27.jpg";
-import image9 from "../public/images/photography/personal/pigs.jpg";
+import image4 from "../public/images/photography/personal/lollapalooza-in-motion/1.jpg";
+import image5 from "../public/images/photography/personal/as-they-remember-us/1.jpg";
+import image6 from "../public/images/photography/event/hs-nashville-2023/27.jpg";
 
 export default function HomePage() {
     return (
@@ -36,7 +33,7 @@ export function HomeContent() {
                     plugins={[Inline]}
                     // open={true}
                     // close={() => setOpen(false)}
-                    slides={[image1, image2, image3, image4, image5, image6, image7, image8, image9]}
+                    slides={[image4, image2, image3, image1, image5, image6]}
                     styles={{ container: { backgroundColor: "rgba(255, 0, 0, 0)" } }}
                     render={{ slide: NextJsImage }}
                 />
